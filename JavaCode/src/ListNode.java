0cdc6dee778992eede0468c736455b28fca1636e
zhangquankang <howtoknow@qq.com>
2019-03-09 23:15:04 +0800

    #000001 单链表反转

---
+++ JavaCode/src/ListNode.java
@@ -0,0 +1,11 @@
+/**
+ * 模拟链表节点
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x){
+        val = x;
+    }
+}
---
+++ JavaCode/src/ReverseList.java
@@ -0,0 +1,23 @@
+/**
+ * 反转链表：
+ * 输入: 1->2->3->4->5->NULL
+ * 输出: 5->4->3->2->1->NULL
+ */
+public class ReverseList {
+
+    public ListNode Solution(ListNode head) {
+        ListNode preNode = null;
+        ListNode nowNode = head;
+        ListNode nextNode = null;
+        while(nowNode != null){
+            //下一节点赋值
+            nextNode = nowNode.next;
+            //反转节点
+            nowNode.next = preNode;
+            //pre和now节点后移，准备下一次操作
+            preNode = nowNode;
+            nowNode = nextNode;
+        }
+        return preNode;
+    }
+}
