839c1b9295c578e260fe3c73d5b10c87f096c03d
zhangquankang <howtoknow@qq.com>
2019-03-21 22:47:19 +0800

    #000003 判断链表是否有环

---
+++ JavaCode/src/List/HasCycle.java
@@ -0,0 +1,26 @@
+package List;
+
+/**
+ * 判断一个链表中是否有环
+ * 使用两个快慢节点，如果慢节点追上快节点，说明有环
+ */
+public class HasCycle {
+    public boolean hasCycle(ListNode head){
+        if(head == null){
+            return false;
+        }
+        ListNode node = head;
+        ListNode fastNode = head;
+        while(node != null){
+            if(fastNode == null || fastNode.next == null){
+                return false;
+            }
+            fastNode = fastNode.next.next;
+            node = node.next;
+            if(fastNode == node){
+                return true;
+            }
+        }
+        return false;
+    }
+}
